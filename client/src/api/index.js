864df9608d9b18d47363a6e7fe3f5decb9767769
Kubrianity <kubraaozerenn@gmail.com>
2023-01-22 02:59:32 +0300

    Update api requests

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,9 @@ import axios from 'axios'
 
 const url = 'http://localhost:5000/todos'
 
-export const fetchTodos = () => axios.get(url)
+const fetchTodos = () => axios.get(url)
+const createTodo = (newTodo) => axios.post(url, newTodo)
+const updateTodo = (id, updatedTodo) => axios.patch(`${url}/${id}`, updatedTodo)
+const deleteTodo = (id) => axios.delete(`${url}/${id}`)
+
+export { fetchTodos, createTodo, updateTodo, deleteTodo } 
