094b65bf98893fcd310aabd38588dd3ac11c6ee5
Kubrianity <kubraaozerenn@gmail.com>
2023-01-22 02:51:54 +0300

    Fix incorrect  middleware order

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ import todosRouter from './routes/todos.js'
 const app = express()
 
 app.use(cors())
-app.use('/todos', todosRouter)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use('/todos', todosRouter)
 
 const PORT = process.env.PORT || 5000
 
