3035974aad611a99ccc17ef76c59056aaa60fda4
Kubrianity <kubraaozerenn@gmail.com>
2023-01-22 22:43:42 +0300

    Include Provider to wrap the root component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import App from './App'
 import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux";
+import { store } from './store/store'
 
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
-root.render(<App />)
+root.render(
+  <Provider store = {store} >
+    <App />
+  </Provider>
+)
 
 export default App
