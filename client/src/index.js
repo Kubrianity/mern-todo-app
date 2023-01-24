e1f1e60eb4cec1d2e325f762925ec5e203287336
Kubrianity <kubraaozerenn@gmail.com>
2023-01-24 22:30:12 +0300

    Install and import react router

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import App from './App'
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux";
 import { store } from './store/store'
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
 root.render(
   <Provider store = {store} >
-    <App />
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
   </Provider>
 )
 
