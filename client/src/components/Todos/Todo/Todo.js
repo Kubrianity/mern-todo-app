53bddb9ba9d860b0d78c1ad8db2410cb3ce76903
Kubrianity <kubraaozerenn@gmail.com>
2023-01-30 00:36:55 +0300

    Implement navigation to the edit form

--- client/src/components/Todos/Todo/Todo.js
+++ client/src/components/Todos/Todo/Todo.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { deleteTodo } from '../../../features/todos/todoSlice'
+import { useNavigate } from 'react-router-dom'
 
 const Todo = ({ todo }) =>  {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const handleClick = (e) => {
     e.preventDefault()
     dispatch(deleteTodo(todo._id))
   }
+  const handleEdit = (id) => {
+    navigate(`/todos/${id}`)
+  }
+
   return (
     <div>
       <p>{ todo.text }</p>
       <button type="submit" onClick = {handleClick}>Delete</button>
+      <button type="button" onClick = {() => handleEdit(todo._id)}>Edit</button>
     </div>
   )
 }
