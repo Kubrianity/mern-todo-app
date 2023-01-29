5a066bef5ce423c00600903987a6555a3a89f713
Kubrianity <kubraaozerenn@gmail.com>
2023-01-30 00:21:29 +0300

    Update parameters, remove semicolons

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -11,11 +11,11 @@ const getTodos = async (req,res) => {
 
 const getTodo = async (req, res) => {
   try {
-    const { id: _id } = req.params;
-    const todo = await Todo.findById(_id);
-    res.status(200).json(todo);
+    const { id } = req.params
+    const todo = await Todo.findById({_id: id})
+    res.status(200).json(todo)
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message })
   }
 }
 
@@ -31,21 +31,21 @@ const createTodo = async (req,res) => {
 }
 
 const updateTodo = async (req, res) => {
-  const { id: _id } = req.params;
-  const todo = req.body;
+  const { id } = req.params
+  const todo = req.body
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(_id, todo, { new: true });
-    res.status(200).json(updatedTodo);
+    const updatedTodo = await Todo.findByIdAndUpdate({_id: id }, todo, { new: true })
+    res.status(200).json(updatedTodo)
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
 }
 
 const deleteTodo = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params
   try {
-    const deletedTodo = await Todo.findByIdAndRemove(_id);
-    res.status(200).json(deletedTodo);
+    const deletedTodo = await Todo.findByIdAndRemove({_id: id})
+    res.status(200).json(deletedTodo)
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
